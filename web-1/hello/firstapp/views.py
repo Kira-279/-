4bbb6ba4c604e170c2088348f2545bd44c4875c6
Kira-279 <kirileto777@gmail.com>
2024-11-16 20:17:31 +0300

    лз-5

--- web-1/hello/firstapp/views.py
+++ web-1/hello/firstapp/views.py
@@ -1,10 +1,50 @@
-from django.shortcuts import render 
-from django.http import HttpResponse 
+# from django.shortcuts import render 
+# from django.http import HttpResponse
 
 # Create your views here. 
+
+# def index(request): 
+#     return HttpResponse("<h2>Глaвнaя</h2>") 
+
+# def about(request): 
+#     return HttpResponse("<h2>0 сайте</h2>") 
+
+# def contact(request): 
+#     return HttpResponse("<h2>Koнтaкты</h2>") 
+
+# def products(request, productid): 
+#     category = request.GET.get("cat", "11") 
+#     output = "<h2>Продукт № {0}  Категория: {1}</h2>" .format(productid, category) 
+#     return HttpResponse(output) 
+
+# def users(request): 
+#     id = request.GET.get("id", 1) 
+#     name = request.GET.get("name", "Максим") 
+#     output = "<h2>Пользователь</h2><h3>id: {0}  Имя: {1}</h3 >" .format(id, name) 
+#     return HttpResponse(output) 
+
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect 
+
 def index(request): 
-    return HttpResponse("<h2>Глaвнaя</h2>") 
+    return HttpResponse("Index") 
 def about(request): 
-    return HttpResponse("<h2>0 сайте</h2>") 
+    return HttpResponse("About") 
 def contact(request): 
-    return HttpResponse("<h2>Koнтaкты</h2>") 
+    return HttpResponseRedirect("/about") 
+def details(request): 
+    return HttpResponsePermanentRedirect("/") 
+
+#from django.http import * 
+
+# def m304(request): 
+#   return HttpResponseNotModified() 
+# def m400(request): 
+#   return HttpResponseBadRequest("<h2>Bad Request</h2>") 
+# def m403(request): 
+#   return HttpResponseForЬidden ( "<h2>ForЬidden</h2>") 
+# def m404(request): 
+#   return HttpResponseNotFound("<h2>Not Found</h2>") 
+# def m405(request): 
+#   return HttpResponseNotAllowed("<h2>Method is not allowed</h2>") 
+# def m410(request): 
+#   return HttpResponseGone("<h2>Content is no longer here</h2>") 
--- web-1/hello/hello/urls.py
+++ web-1/hello/hello/urls.py
@@ -14,11 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin 
 from django.urls import path 
 from django.urls import re_path 
 from firstapp import views 
+
+#urlpatterns = [ 
+#    path('', views.index), 
+#    re_path(r'^about', views.about), 
+#    re_path(r'^contact', views.contact), 
+#    path('products/<int:productid>/', views.products), 
+#    path('users/', views.users), 
+#] 
+
 urlpatterns = [ 
-    re_path (r'^about/contact/', views.contact), 
-    re_path(r'^about', views.about), 
-    path ('', views. index), 
-]  
+path('', views.index), 
+path('about/', views.about), 
+path('contact/', views.contact), 
+path('details/', views.details), 
+]
