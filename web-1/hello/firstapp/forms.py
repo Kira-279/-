77c98ec457842f7325a796da6f48d7e3945a53e8
Kira-279 <kirileto777@gmail.com>
2024-11-21 00:00:44 +0300

    лз-8

--- web-1/hello/firstapp/forms.py
+++ web-1/hello/firstapp/forms.py
@@ -3,5 +3,19 @@ from django import forms
 class UserForm(forms.Form):
 #    name = forms.CharField()
 #    age = forms.IntegerField()
-    name = forms.CharField(label='Имя клиента')
-    age = forms.IntegerField(label='Возраст клиента')
+#    name = forms.CharField(label='Имя клиента')
+#    age = forms.IntegerField(label='Возраст клиента')
+
+#    basket = forms.BooleanField(label="Положить товар в корзину", required=False)
+#  
+#    vyb = forms.NullBooleanField(label="Вы поедете в Сочи этим летом?")
+#    name = forms.CharField(label="Имя клиента", required=False, max_length=15, help_text="ФИO не более 15 символов")
+#    email = forms.EmailField(label="Электронный адрес", help_text="Обязательный символ - @")
+#    ip_adres = forms.GenericIPAddressField(label="IP адрес", help_text=" Пример формата 192.0.2.0") 
+#    reg_text = forms.RegexField(label="Текст", regex="^[0-9][A-F][0-9]$")
+#    slug_text = forms.SlugField(label="Введите текст")
+#    url_text = forms.URLField(label="Введите URL", help_text="Например http://www.google.com")
+#    uuid_text = forms.UUIDField(label="Введите UUID", help_text="Формат xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx") 
+#       combo_text = forms.ComboField(label="Введите URL", fields=[forms.URLField(), forms.CharField(max_length=20)])
+#    file_path = forms.FilePathField(label="Выберите файл", path="C:", allow_files="True", allow_folders="True")
+    file = forms.FileField(label="Файл")
--- web-1/hello/firstapp/views.py
+++ web-1/hello/firstapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import *
 from django.template.response import TemplateResponse 
 from .forms import UserForm
+from .forms import UserForm 
+from django.shortcuts import render 
 
 # def index(request): 
 #    return TemplateResponse(request, "firstapp/home.html")
@@ -41,12 +43,12 @@ from .forms import UserForm
 #    return render(request, "firsatapp/index.html", {"form": userform})
 
 def index(request):
-    if request.method == "POST":
-        name = request.POST.get("name") # получить значения поля Имя
-        age = request.POST.get("age") # значения поля Возраст
-        output = "<h2>Пользователь</h2><h3>Имя - {0}, Возраст – {1}</h3>".format(name, age)
-        return HttpResponse(output)
-    else:
+#    if request.method == "POST":
+#        name = request.POST.get("name") # получить значения поля Имя
+#        age = request.POST.get("age") # значения поля Возраст
+#        output = "<h2>Пользователь</h2><h3>Имя - {0}, Возраст – {1}</h3>".format(name, age)
+#        return HttpResponse(output)
+#    else:
         userform = UserForm()
         return render(request, "firstapp/index.html", {"form": userform})
 
